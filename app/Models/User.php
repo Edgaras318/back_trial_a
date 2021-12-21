8829cd7b61b54711125c74d913887f532e4be329
Edgaras <edgaras318@gmail.com>
2021-12-21 09:42:10 +0100

    feat: user CRUD api

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'role',
         'email',
+        'picture',
+        'phone',
         'password',
     ];
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['boss', 'developer', 'intern']);
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('picture')->nullable();
+            $table->string('phone')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
